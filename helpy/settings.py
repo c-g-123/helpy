ee0949feb36a33e0078c52c1688b8a9629069744
c-g <no-email>
2026-03-03 21:22:30 +0000

    feat: user can login
    Most views are restricted until a user logs in. Users can also logout.

--- helpy/settings.py
+++ helpy/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+LOGIN_URL = 'core:login'
+
 # =====================================================================================================
 
 
