247836a483221b35793ead91d293924fed468b58
Kunter Cetin <3075044C@student.gla.ac.uk>
2026-03-07 16:38:06 +0000

    feat: load and save user settings in account view

--- core/views/other.py
+++ core/views/other.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from core.models import UserSettings
+from core.forms import UserSettingsForm
 
 def index(request):
     return render(request, 'core/index.html')
@@ -8,4 +10,18 @@ def index(request):
 
 @login_required
 def account(request):
+    user_settings, created = UserSettings.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = UserSettingsForm(request.POST, instance=user_settings)
+
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserSettingsForm(instance=user_settings)
+    
+    context = {
+        'form': form,
+    }
+    
     return render(request, 'core/settings.html')
