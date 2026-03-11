59393cea6213a1c5d3e93bc4067fceda1ffb8b55
c-g <no-email>
2026-03-11 21:12:37 +0000

    fix: remove half-finished agenda add task feature

--- core/forms.py
+++ core/forms.py
@@ -71,12 +71,12 @@ class TaskForm(forms.ModelForm):
         })
     )
 
-    set_date = forms.DateTimeField(
+    set_datetime = forms.DateTimeField(
         required=False,
         widget=forms.DateTimeInput(attrs={"type": "datetime-local"})
     )
 
-    due_date = forms.DateTimeField(
+    due_datetime = forms.DateTimeField(
         required=False,
         widget=forms.DateTimeInput(attrs={"type": "datetime-local"})
     )
@@ -87,8 +87,8 @@ class TaskForm(forms.ModelForm):
             "project",
             "name",
             "description",
-            "set_date",
-            "due_date",
+            "set_datetime",
+            "due_datetime",
         ]
 
     def __init__(self, *args, **kwargs):
