48777e4733d3af99ec266bec68264a182a67e54d
Imogen Graham <imogenmcgraham@gmail.com>
2026-03-16 13:15:06 +0000

    got tasks showing up reusing the card task used in agenda, needs testing and formatting

--- core/views/planning/kanban.py
+++ core/views/planning/kanban.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from core.models import Task
 
 
 @login_required
 def kanban(request):
-    return render(request, 'core/planning/kanban.html')
+
+    tasks_todo = Task.objects.filter(status=Task.Status.TODO)
+    tasks_in_progress = Task.objects.filter(status=Task.Status.IN_PROGRESS)
+    tasks_done= Task.objects.filter(status=Task.Status.DONE)
+
+    context = {
+        'tasks_todo': tasks_todo,
+        'tasks_in_progress': tasks_in_progress,
+        'tasks_done': tasks_done
+        }
+
+    return render(request, 'core/planning/kanban.html', context)
